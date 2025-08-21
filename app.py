a96fba69a6485aa47d1e3c667e1cd3106f2f53d7
G Vignesh <g.vignesh@bcah.christuniversity.in>
2025-08-21 18:11:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ def upload_and_execute():
         <h3>Error:</h3>
         <pre>{{ error }}</pre>
     {% endif %}
-    <p>Hint: Buy from the website.</p>
+   
     ''', output=output, error=error)
 
 # Do not call app.run(), as Vercel runs the app serverlessly
